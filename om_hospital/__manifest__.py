0f3d9fce22348a1948cff179e2876b9f6b226061
caioalexandre6 <caioalexandreoliveiraalves@gmail.com>
2019-11-22 14:36:24 -0300

    First Odoo Module

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "Hospital Management",
+    'summary': "Gerencia seu hospital",
+    'category': 'Extra Tools',
+    'website': 'www.naoexiste.com.br',
+    'author': 'Caio Alexandre',
+    'description': """
+Gerenciador de Hospital
+=======================
+Muito bom pra não maatar paciente atoa
+""",
+    'category': 'Technical Settings',
+    'version': '1.0',
+    'depends': [],
+    'data': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'OEEL-1',
+}
